ea2b01e50e9f243e18b0c930efa73bb4ae0caec4
vitali <vitali.slepnyov@gmail.com>
2022-09-26 23:55:39 +0300

    feat: подключить роутеры

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
-const app = express();
+
 const { PORT = 3000 } = process.env;
+const app = express();
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// на роуты пользователя и карточек
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
+  console.log(`App работает в порте ${PORT}`);
 })
 
