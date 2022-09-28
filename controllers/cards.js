c8616e74e49a08386a068435f7936dbb170c2cca
vitali <vitali.slepnyov@gmail.com>
2022-09-28 23:47:01 +0300

    feat: добавить обработчики в контроллеры cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,7 +1,7 @@
 const Card = require('../models/card');
 
-
 module.exports.getAllCards = (req, res) => {
+  console.warn('получены все карточки'); //!
   Card.find({})
     .then(cards => res.send({ data: cards }))
     .catch(err => res.status(500).send({ message: err.message }))
@@ -9,16 +9,37 @@ module.exports.getAllCards = (req, res) => {
 
 
 module.exports.createCard = (req, res) => {
-  console.log(req.user._id); // _id станет доступен //!
+  console.warn('сделана новая карточка'); //!
+  const owner = req.user._id;
   const { name, link } = req.body;
+  console.warn(owner); //!
 
-  Card.create({ name, link })
+  Card.create({ name, link, owner })
     .then(newCard => res.send({ data: newCard }))
     .catch(err => res.status(500).send({ message: err.message }))
 };
 
 
 module.exports.deleteCardOnId = (req, res) => {
-  //! функция удаления
+  console.warn('карточка была удалена'); //!
+  const id = req.params.cardId;
+  Card.findByIdAndRemove(id)
+    .then(card => res.send({ data: card }))
+    .catch(err => res.status(500).send({ message: err.message }))
 }
 
+
+module.exports.putLikeCard = (req, res) => {
+  console.warn('Поставлен лайк'); //!
+  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .then(card => res.send({ data: card }))
+    .catch(err => res.status(500).send({ message: err.message }))
+}
+
+
+module.exports.dislikeCard = (req, res) => {
+  console.warn('Лайк удалён'); //!
+  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .then(card => res.send({ data: card }))
+    .catch(err => res.status(500).send({ message: err.message }))
+}
