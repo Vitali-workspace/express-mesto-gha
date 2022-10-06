86d1064d520c9ada98cee1bb18b4c43a218cc2f8
vitali <vitali.slepnyov@gmail.com>
2022-10-06 23:07:50 +0300

    refactor: изменить схемы

--- models/user.js
+++ models/user.js
@@ -1,19 +1,33 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Исследователь',
   },
   avatar: {
+    type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (item) => validator.isEmail(item),
+      message: 'ошибка в почте',
+    },
+  },
+  password: {
     type: String,
     required: true,
   },
