1ced2a9e8bc78694aacbff3588ea9ce9b3c32df9
vitali <vitali.slepnyov@gmail.com>
2022-10-10 23:48:07 +0300

    feat: добавить схему проверки  email, password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,5 +33,26 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      } else {
+
+        return bcrypt.compare(password, user.password)
+          .then((matched) => {
+            if (!matched) {
+              return Promise.reject(new Error('Неправильные почта или пароль'));
+            } else {
+              return user;
+            }
+          }).catch(err => console.log('ошибка схемы проверки пользователя'));
+      }
+    })
+}
+
 
 module.exports = mongoose.model('user', userSchema);
