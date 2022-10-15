5574f44df116d8a730fa0ae211ddb57bb0cfdab1
vitali <vitali.slepnyov@gmail.com>
2022-10-16 00:40:39 +0300

    feat: добавить централизованный обработчик ошибок

--- app.js
+++ app.js
@@ -1,36 +1,45 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 const cors = require('cors');
+
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
-const { login, createUser } = require('./controllers/users');
+const authentication = require('./routes/authentication');
 const auth = require('./middlewares/auth');
+const PageNotFoundError = require('./errors/PageNotFoundError');
 
-const STATUS_NOT_FOUND = 404;
 
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(cors());
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = { _id: '633290e5121aa47983421da5' };
-  next();
-});
-
-app.post('/signin', login);
-app.post('/signup', createUser);
-
+app.use('/', authentication);
 app.use(auth);
 app.use(routerUsers);
 app.use(routerCards);
 
-app.use('/*', (req, res) => {
-  res.status(STATUS_NOT_FOUND).send({ message: 'Запрошенные данные не найдены' });
+app.use('*', (req, res, next) => {
+  next(new PageNotFoundError('Запрошенные данные не найдены'));
+});
+
+// обработка ошибок celebrate
+app.use(errors());
+
+// централизованный обработчик ошибок
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send(
+    { message: statusCode === 500 ? 'На сервере произошла ошибка' : message },
+  );
+  next();
 });
 
 app.listen(PORT, () => {
